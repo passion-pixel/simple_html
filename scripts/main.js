1fad9dd22aeb662aaee5116fbeb4f7c1837ed39f
Natalie Contreras <natalie.isabel.contreras.a@gmail.com>
2018-02-12 05:22:07 -0800

    done for the night! almost all specs completed

--- scripts/main.js
+++ scripts/main.js
@@ -54,10 +54,10 @@ function transitionSlides () {
   $('.carousel-indicators li[data-slide-number="' + currentSlideIndex + '"]').addClass('active');
 }
 
-
 // --------------------------
 // Lightbox -----------------
 // --------------------------
+
 $('.carousel').on('click', 'img', function () {
   // Clear Timer for Carousel so that it stops switching slides
   clearInterval(carouselTimer);
@@ -65,7 +65,10 @@ $('.carousel').on('click', 'img', function () {
   // Find out which image the user wants to view in the lightbox
   var imageSrc = $(this).attr('src');
 
-  $('.lightbox img').attr('src', imageSrc);
+  $('.lightbox img').attr({
+  src: imageSrc,
+  class: "lightbox"
+});
 
   // Fade in lightbox (as a flex container)
   $('.lightbox').css('display', 'flex').hide().fadeIn(300, function () {
@@ -101,96 +104,4 @@ setInterval(function () {
   });
 }, 7000);
 
-});
-
-
-// --------------------------
-// Smooth Scroll Nav
-// --------------------------
-
-// Do it when someone clicks a nav link
-$('.navbar-nav a, .logo a').on('click', function(e) {
-  // prevent the standard link operation on click
-  e.preventDefault();
-  // use the href of the link to identify what
-  // section to scroll to
-  var thisTarget = $(this).attr('href');
-  // get that section's top offset
-  var targetOffset = $(thisTarget).offset().top;
-  // use jQuery.animate() to animate the body's
-  // scrollTop to the targetOffest
-  $('body').animate({
-    scrollTop: targetOffset
-  }, 600);
-});
-
-
-// -----------------------------
-// Highlight Nav Links on Scroll
-// -----------------------------
-// cache the navigation links
-var $navigationLinks = $('.navbar-nav a');
-// cache (in reversed order) the sections
-var $sections = $($("section, header").get().reverse());
-
-// map each section id to their corresponding navigation link
-var sectionIdTonavigationLink = {};
-$sections.each(function() {
-    var id = $(this).attr('id');
-    sectionIdTonavigationLink[id] = $('.navbar-nav a[href="#' + id + '"]');
-});
-
-// throttle function, enforces a minimum time interval
-function throttle(fn, interval) {
-    var lastCall, timeoutId;
-    return function () {
-        var now = new Date().getTime();
-        if (lastCall && now < (lastCall + interval) ) {
-            // if we are inside the interval we wait
-            clearTimeout(timeoutId);
-            timeoutId = setTimeout(function () {
-                lastCall = now;
-                fn.call();
-            }, interval - (now - lastCall) );
-        } else {
-            // otherwise, we directly call the function
-            lastCall = now;
-            fn.call();
-        }
-    };
-}
-
-function highlightNavigation() {
-    // get the current vertical position of the scroll bar
-    var scrollPosition = $(window).scrollTop();
-
-    // iterate the sections
-    $('section').each(function() {
-        var currentSection = $(this);
-        // get the position of the section
-        var sectionTop = currentSection.offset().top - 71;
-
-        // if the user has scrolled over the top of the section
-        if (scrollPosition >= sectionTop) {
-            // get the section id
-            var id = currentSection.attr('id');
-            // get the corresponding navigation link
-            var $navigationLink = sectionIdTonavigationLink[id];
-            // if the link is not current
-            if (!$navigationLink.hasClass('current')) {
-                // remove .current class from all the links
-                $navigationLinks.removeClass('current');
-                // add .current class to the current link
-                $navigationLink.addClass('current');
-            }
-            // we have found our section, so we return false to exit the each loop
-            return false;
-        }
-    });
-}
-
-$(window).scroll( throttle(highlightNavigation, 100) );
-$('.hamburger').on('click', function (e) {
-  e.preventDefault();
-  $('.nav-link').toggleClass('open-menu');
 });
